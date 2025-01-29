c820a1e40f0620c82cd7caf7a9f6d47b67466b24
adrien.bolling <bolling.adrien@gmail.com>
2025-01-29 17:04:22 +0100

    Update run script to activate virtual environment and run new Streamlit app

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -2,10 +2,10 @@
 
 # Activate your virtual environment if necessary
 # Uncomment and modify the following line if you use a virtual environment
-source ../.venv/bin/activate
+source .venv/bin/activate
 
 # Run the script
-streamlit run ../st_app/Main.py
+streamlit run ./src/st_app/new_main.py
 
 # Optional: Deactivate the virtual environment after the app is stopped
 # deactivate
